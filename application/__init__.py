6e739aee6e891aeb304fde5ca5014040b3be14a4
fxbrartur <fxbrartur@gmail.com>
2022-08-23 00:09:13 -0300

    first deploy

---
+++ application/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_dropzone import Dropzone
+import os
+
+app = Flask(__name__)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+app.config['SECRET_KEY'] = '47cc946a2fd8721de8b9025ce1b33c39e26f9e0c'
+
+app.config.update(
+    UPLOADED_PATH=os.path.join(dir_path, "static"),
+    DROPZONE_ALLOWED_FILE_TYPE='image',
+    DROPZONE_MAX_FILE_SIZE=3,
+    DROPZONE_MAX_FILES=1
+)
+
+app.config['DROPZONE_REDIRECT_VIEW'] = 'decoded'
+
+dropzone = Dropzone(app)
+
+
+from application import routes
---
+++ application/routes.py
@@ -0,0 +1,66 @@
+from application import app
+from application.forms import QRCodeData
+from flask import render_template, request, redirect, url_for
+import qrcode
+import secrets
+import cv2
+import os
+
+
+decoded_info = ""
+
+
+@app.route("/home")
+def index():
+    return render_template("home.html", title="Home Page")
+
+
+@app.route("/genqrcode", methods=["POST", "GET"])
+def index_page():
+    form = QRCodeData()
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            data = form.data.data
+            image_name = f"{secrets.token_hex(10)}.png"
+            qrcode_location = f"{app.config['UPLOADED_PATH']}/{image_name}"
+
+            try:
+                my_qrcode = qrcode.make(str(data))
+                my_qrcode.save(qrcode_location)
+            except Exception as e:
+                print(e)
+            return render_template("generated_qrcode.html", title="Generated", image=image_name)
+    else:
+        return render_template("genqrcode.html", title="Generation Page", form=form)
+
+
+@app.route("/decode", methods=["GET", "POST"])
+def upload():
+    if request.method == 'POST':
+        global decoded_info
+        f = request.files.get('file')
+        filename, extension = f.filename.split(".")
+        generated_filename = secrets.token_hex(20) + f".{extension}"
+
+        file_location = os.path.join(app.config['UPLOADED_PATH'], generated_filename)
+        f.save(file_location)
+        print(file_location)
+
+        img = cv2.imread(file_location)
+        det = cv2.QRCodeDetector()
+
+        val, pts, st_code = det.detectAndDecode(img)
+        print(val)
+
+        os.remove(file_location)
+        decoded_info = val
+
+    else:
+        return render_template("upload.html", title="Decoding")
+
+
+@app.route("/decoded", methods=["GET"])
+def decoded():
+    global decoded_info
+    return render_template("decoded.html", title="Decoded", data=decoded_info)
